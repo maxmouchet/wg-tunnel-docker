db16b348fb5add6ab0b98e2d0f2e79575ab3c1ea
Maxime Mouchet <max@maxmouchet.com>
2022-10-06 12:08:40 +0200

    implement kill-switch to prevent leaks

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,8 @@ ENV DEBIAN_FRONTEND=noninteractive
 
 RUN apt-get update \
     && apt-get install --no-install-recommends --yes \
-        ca-certificates \
-        curl \
         iproute2 \
-        iputils-ping \
+        iptables \
         wireguard-go \
         wireguard-tools \
     && rm -rf /var/lib/apt/lists/*
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
 set -euo pipefail
 
+: "${EXCLUDED_NETWORK:=172.16.0.0/12}"
 : "${WG_FWMARK:=1234}"
 : "${WG_INTERFACE:=wg0}"
 : "${WG_PEER_ALLOWED_IPS:=0.0.0.0/0,::0/0}"
 : "${WG_TABLE:=2468}"
+
+# Block non-local and non-wireguard as early as possible to prevent leaks.
+# We use iptables-legacy for compatibility with Synology DSM.
+alias iptables=iptables-legacy
+alias ip6tables=ip6tables-legacy
+
+# Drop everything by default.
+iptables  --policy OUTPUT DROP
+ip6tables --policy OUTPUT DROP
+
+# Allow traffic going towards an IP address assigned to this host (e.g. 127.0.0.1, ::1).
+iptables  --append OUTPUT --match addrtype --dst-type LOCAL --jump ACCEPT
+ip6tables --append OUTPUT --match addrtype --dst-type LOCAL --jump ACCEPT
+
+# Allow encrypted WireGuard traffic.
+iptables  --append OUTPUT --match mark --mark "${WG_FWMARK}" --jump ACCEPT
+ip6tables --append OUTPUT --match mark --mark "${WG_FWMARK}" --jump ACCEPT
+
+# Allow traffic going through the WireGuard interface.
+iptables  --append OUTPUT --out-interface "${WG_INTERFACE}" --jump ACCEPT
+ip6tables --append OUTPUT --out-interface "${WG_INTERFACE}" --jump ACCEPT
+
+# Allow traffic towards Docker internal networks, or any custom networks.
+for network in ${EXCLUDED_NETWORK}; do
+    iptables  --append OUTPUT --dst "${network}" --jump ACCEPT
+done
 
 wireguard "${WG_INTERFACE}"
 
